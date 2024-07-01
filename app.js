f294d84a5886b78b2dec18aedd9db51fb1b77cd6
Juan Pablo F. H. <20206470@garzas.uatx.mx>
2024-07-01 08:13:55 -0600

    Conclusión de la clase 3.6

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // let parrafo = document.querySelector('p');
 // parrafo.innerHTML = 'Indica un número del 1 al 10';
 
-let numeroSecreto = generarNumeroSecreto();
+let numeroSecreto;
+let intentos;
 
-console.log(numeroSecreto);
 
-asignarTextoElemento('h1', 'Juego del número secreto'); //Hoisiting
-asignarTextoElemento('p', 'Indica un número del 1 al 10');
+function condicionesIniciales() {
+  asignarTextoElemento('h1', 'Juego del número secreto'); //Hoisiting
+  asignarTextoElemento('p', 'Indica un número del 1 al 10.');
+  numeroSecreto = generarNumeroSecreto();
+  intentos = 1;
+}
 
 function asignarTextoElemento(elemento, texto) {
   let elementoHTML = document.querySelector(elemento);
@@ -16,12 +20,49 @@ function asignarTextoElemento(elemento, texto) {
 function verificarIntento() {
   let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
 
-  console.log(numeroSecreto);
-  console.log(typeof(numeroSecreto));
-  console.log(numeroDeUsuario);
-  console.log(typeof(numeroDeUsuario));
+  // console.log(numeroSecreto);
+  // console.log(typeof(numeroSecreto));
+  // console.log(numeroDeUsuario);
+  // console.log(typeof(numeroDeUsuario));
+
+  if (numeroDeUsuario === numeroSecreto) {
+    asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos == 1) ? 'vez' : 'veces'}.`);
+
+    document.getElementById('reiniciar').removeAttribute('disabled');
+  } else {
+    //El usuario no acertó.
+    limpiarCajaDeTexto();
+
+    if (numeroDeUsuario > numeroSecreto) {
+      asignarTextoElemento('p', 'El número secreto es menor.');
+    } else {
+      asignarTextoElemento('p', 'El número secreto es mayor.');
+    }
+
+    intentos++;
+  }
 }
 
 function generarNumeroSecreto() {
   return Math.floor(Math.random() * 10) + 1;
 }
+
+function limpiarCajaDeTexto() {
+  document.querySelector('#valorUsuario').value = '';
+}
+
+function reiniciarJuego(){
+  // Limpiar la caja
+  limpiarCajaDeTexto();
+
+  // Indicar mensaje de intervalo de números
+  // Generar el nuevo aleatorio
+  // Inicializar el número de intentos
+  condicionesIniciales();
+
+  // Desabilitar el botón de nuevo juego
+  document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+condicionesIniciales();
+console.log(numeroSecreto);
