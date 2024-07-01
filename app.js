fa5a67597460e50c2eb79212c26c4b7f369c7b78
Juan Pablo F. H. <20206470@garzas.uatx.mx>
2024-07-01 07:00:35 -0600

    Conclusión de la clase 2.6

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // let parrafo = document.querySelector('p');
 // parrafo.innerHTML = 'Indica un número del 1 al 10';
 
+let numeroSecreto = generarNumeroSecreto();
+
+console.log(numeroSecreto);
+
 asignarTextoElemento('h1', 'Juego del número secreto'); //Hoisiting
 asignarTextoElemento('p', 'Indica un número del 1 al 10');
 
@@ -9,6 +13,15 @@ function asignarTextoElemento(elemento, texto) {
   elementoHTML.innerHTML = texto;
 }
 
-function intentoDeUsuario() {
-  alert('Click desde el botón');
+function verificarIntento() {
+  let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+  console.log(numeroSecreto);
+  console.log(typeof(numeroSecreto));
+  console.log(numeroDeUsuario);
+  console.log(typeof(numeroDeUsuario));
+}
+
+function generarNumeroSecreto() {
+  return Math.floor(Math.random() * 10) + 1;
 }
